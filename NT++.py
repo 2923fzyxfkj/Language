338a0d0ea2ffc1eeed1418690c2cd745be3cc1c2
2923fzyxfkj <162860026+2923fzyxfkj@users.noreply.github.com>
2024-10-20 13:09:52 +0800

    Add files via upload

---
+++ NT++.py
@@ -0,0 +1,81 @@
+import os
+print('explain:2605368981@qq.com')
+print("use python write")
+print("Open source ")
+def language():
+    global python_install_all
+    def python_install_all():
+            import requests
+            import re
+            import time
+            import os
+            import sys
+            import random
+            import threading
+            import json
+            import base64
+            import hashlib
+            import binascii
+            import urllib.parse
+            import datetime
+            import subprocess
+            import platform
+            import psutil
+            import socket
+            import getpass
+def cmd(Command):
+    while 1:
+        print("Permission:Run_Permission")
+        command = Command
+        os.system(command)
+        if command == "exit(1)":
+            main()
+
+def main():
+    while 1:
+        a = input('>>>')
+        if a == 'python.install.all':
+            print('loading...')
+            print('''all:
+                  os.system('pip install requests')
+            os.system('pip install re')
+            os.system('pip install time')
+            os.system('pip install os')
+            os.system('pip install sys')
+            os.system('pip install random')
+            os.system('pip install threading')
+            os.system('pip install json')
+            os.system('pip install base64')
+            os.system('pip install hashlib')
+            os.system('pip install binascii')
+            os.system('pip install urllib.parse')
+            os.system('pip install datetime')
+            os.system('pip install subprocess')
+            os.system('pip install platform')
+            os.system('pip install psutil')
+            os.system('pip install socket')
+            os.system('pip install gitpass')
+                ''')
+            os.system('pip install requests')
+            os.system('pip install re')
+            os.system('pip install time')
+            os.system('pip install os')
+            os.system('pip install sys')
+            os.system('pip install random')
+            os.system('pip install threading')
+            os.system('pip install json')
+            os.system('pip install base64')
+            os.system('pip install hashlib')
+            os.system('pip install binascii')
+            os.system('pip install urllib.parse')
+            os.system('pip install datetime')
+            os.system('pip install subprocess')
+            os.system('pip install platform')
+            os.system('pip install psutil')
+            os.system('pip install socket')
+            os.system('pip install gitpass')
+            python_install_all()
+        if (a == cmd):
+            COMMAND = input("Command>")
+            cmd(COMMAND)
+main()
